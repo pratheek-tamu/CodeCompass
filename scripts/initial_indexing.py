d04a93d96e61fef7aa000c6dbd4146074c0d6cf4
Shahin John J S <ivin-98@tamu.edu>
2025-03-31 16:12:19 -0500

    docker and setup script change

--- scripts/initial_indexing.py
+++ scripts/initial_indexing.py
@@ -1,19 +1,28 @@
+import sys
+import os
+
 from src.utils.config_loader import load_config
 from src.utils.logging_utils import setup_logger
 
 from src.utils.mongodb_utils import get_mongodb_client, insert_metadata
 from src.utils.graphdb_utils import create_graph, save_graph
+from src.ingestion.ingestion_manager import IngestionManager
 
 logger = setup_logger()
 config = load_config()
 
-mongo_client = get_mongodb_client(config["mongodb"]["uri"])
 metadata_example = [
-    {"function_name": "processData", "file_path": "src/utils/data_processing.py", "api_reference": "docs/api/data_processing.md"},
+    {"function_name": "processData", "file_path": "src/utils/data_processing.py", "api_reference": "docs/api/data_processing.md"}
 ]
-insert_metadata(mongo_client, config["mongodb"]["database"], config["mongodb"]["metadata_collection"], metadata_example)
+insert_metadata(metadata_example)
 logger.info("Inserted initial metadata into MongoDB.")
 
 graph = create_graph()
-save_graph(graph, config["graphdb"]["graph_storage_path"])
+save_graph(graph)
 logger.info("Created initial NetworkX graph.")
+
+# code for parasing
+repo_path = "data/PyGithub/PyGithub-main/github"
+ingestion = IngestionManager(repo_path)
+ingested_data = ingestion.ingest()
+logger.info("Some data parsing should have happened.")
